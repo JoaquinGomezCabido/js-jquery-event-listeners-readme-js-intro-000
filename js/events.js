b35438e142cfa00db1100c2cb0c3e4f6882f2b23
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 13:10:07 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,9 +11,23 @@ var frameIt = () => {
   $('img').on('load', function() {
     $('img').addClass('tasty')
   })
+  return
 }
 
+var pressIt = () => {
+  $('input').on('keydown', function(key) {
+    if(key.key == "g") {
+      alert("G was pressed!")
+    }
+    return
+  })
+}
 
+var submitIt = () => {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submited now.")
+  })
+}
 
 $(document).ready(function(){
 
