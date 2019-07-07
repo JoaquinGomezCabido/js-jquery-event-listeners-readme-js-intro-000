8b9549b120b640e5cb2a4319ba729c5fc10d0e8d
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 13:06:22 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,20 @@
 //define functions here
 
+var getIt = () => {
+  $('p').on('click', function() {
+    alert("Hey!")
+    return
+  })
+}
+
+var frameIt = () => {
+  $('img').on('load', function() {
+    $('img').classList.add('tasty')
+  })
+}
+
+
+
 $(document).ready(function(){
 
 // call functions here
